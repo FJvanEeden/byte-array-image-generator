fdc03e2d72d92031f4d431173ef53e64ee09e1a2
FJvanEeden <fredz.eeden@gmail.com>
2020-05-07 07:01:00 +0200

    Completed Image generator now uses byte array to build image file.

--- byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
@@ -1,18 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
 
 namespace byteArrayImageGenerator
 {
-  class ImageGenerator : IImageGenerator
+  class ImageGenerator
   {
-    public Image GenerateImage(byte[] stream, int widthInPixels, int heightInPixels, ImageFormat outputImageType)
+    public ImageGenerator()
     {
+    }
 
+    public static bool WriteBitmapFile(string filename, int width, int height, byte[] rawImageData, PixelFormat pixelFormat)
+    {
+      byte[] newData = new byte[rawImageData.Length];
+
+      for (int x = 0; x < rawImageData.Length; x += 4)
+      {
+        byte[] pixel = new byte[4];
+        Array.Copy(rawImageData, x, pixel, 0, 4);
+
+        byte r = pixel[0];
+        byte g = pixel[1];
+        byte b = pixel[2];
+        byte a = pixel[3];
+
+        byte[] newPixel = new byte[] { b, g, r, a };
+
+        Array.Copy(newPixel, 0, newData, x, 4);
+      }
+
+      rawImageData = newData;
+
+      using (var stream = new MemoryStream(rawImageData))
+      using (var bmp = new Bitmap(width, height, pixelFormat))
+      {
+        BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+
+        IntPtr pNative = bmpData.Scan0;
+        Marshal.Copy(rawImageData, 0, pNative, rawImageData.Length);
+
+        bmp.UnlockBits(bmpData);
+
+        bmp.Save(filename, ImageFormat.Png);
+      }
+      return true;
+    }
+
+    // For testing purposes ONLY
+    public static byte[] imageToByteArray(System.Drawing.Image imageIn)
+    {
+      MemoryStream ms = new MemoryStream();
+      imageIn.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+      return ms.ToArray();
+    }
+
+    public static Image byteArrayToImage(byte[] byteArrayIn)
+    {
+      MemoryStream ms = new MemoryStream(byteArrayIn);
+      Image returnImage = Image.FromStream(ms);
+      return returnImage;
     }
   }
 }
--- byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.Remoting.Channels;
 
 namespace byteArrayImageGenerator
 {
@@ -10,7 +9,29 @@ namespace byteArrayImageGenerator
   {
     static public void Main(String[] args)
     {
-      Console.WriteLine("To Test function later.");
+      Console.WriteLine("To Test function.");
+
+      //Test values
+      string folderPath = @"C:\Users\TKK\Documents\Onyx_projects\file.png";
+      var path = @"C:\Users\TKK\Documents\Onyx_projects\7zqm3qahhqo41.png";
+
+      var width = 225;
+      var height = 225;
+      var pixelFormat = PixelFormat.Format32bppArgb;
+      byte[] testArr = { 255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 0, 255, 0, 0, 255, 255 };
+
+
+      //Test #3
+      var success = ImageGenerator.WriteBitmapFile(folderPath, width, height, testArr, pixelFormat);
+
+      if (success == true)
+      {
+        Console.WriteLine("File made");
+      }
+      else
+      {
+        Console.WriteLine("Error");
+      }
     }
   }
 }
