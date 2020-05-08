6f29d4a24e369410cda1ec57f7d2c59e09656746
FJvanEeden <fredz.eeden@gmail.com>
2020-05-08 13:22:27 +0200

    Removed image format input from function and removed need to read array to memory stream.

--- byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
@@ -1,85 +1,55 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.IO;
 using System.Runtime.InteropServices;
 
 namespace byteArrayImageGenerator
 {
-  class ImageGenerator : IEnumerable // Removed class interface
+  class ImageGenerator
   {
-    private object[] _objects; // Test implementation
-
-    public ImageGenerator()
-    {
-      
-    }
-
     // Made static to test
-    public static Image GenerateImageFromByteArr(byte[] rawImageData, int width, int height, ImageFormat imageFormat)
+    public static Image GenerateImageFromByteArr(IEnumerable<byte> rawImageData, int width, int height)
     {
-      byte[] newData = new byte[rawImageData.Length];
+      var x = 0;
+      int i;
+      byte[] pixel = new byte[4];
+      byte[] newData = new byte[width * height * 4];
 
-      for (int x = 0; x < rawImageData.Length; x += 4)
+      foreach (var data in rawImageData)
       {
-        byte[] pixel = new byte[4];
-        Array.Copy(rawImageData, x, pixel, 0, 4);
+        i = x % 4;
+        if (x >= 0 && i == 0)
+        {
+          byte r = pixel[0];
+          byte g = pixel[1];
+          byte b = pixel[2];
+          byte a = pixel[3];
+          byte[] newPixel = new byte[] { b, g, r, a };
+          Array.Copy(newPixel, 0, newData, x, 4);
+        }
+        pixel[i] = data;
+        x++;
+      }
 
+      i = x % 4;
+      if (x >= 0 && i == 0)
+      {
         byte r = pixel[0];
         byte g = pixel[1];
         byte b = pixel[2];
         byte a = pixel[3];
-
         byte[] newPixel = new byte[] { b, g, r, a };
-
         Array.Copy(newPixel, 0, newData, x, 4);
       }
 
-      rawImageData = newData;
-
-      using (var stream = new MemoryStream(rawImageData))
       using (Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
       {
         BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
-
-        IntPtr pNative = bmpData.Scan0;
-        Marshal.Copy(rawImageData, 0, pNative, rawImageData.Length);
+        Marshal.Copy(newData, 0, bmpData.Scan0, newData.Length);
 
         bmp.UnlockBits(bmpData);
-        bmp.Save(stream, imageFormat);
         return (Image)bmp.Clone();
-      }
-    }
-
-    // Test implementation
-    public IEnumerator GetEnumerator()
-    {
-      return new ArrayEnumerator(_objects);
-    }
-
-    private class ArrayEnumerator : IEnumerator
-    {
-      private object[] mValues;
-
-      private int _currentIndex = -1;
-
-      public ArrayEnumerator(object[] values)
-      {
-        mValues = values;
-      }
-
-      public bool MoveNext()
-      {
-        _currentIndex++;
-        return (_currentIndex < mValues.Length);
-      }
-
-      public object Current { get; private set; } = 0;
-      
-      public void Reset()
-      {
-        _currentIndex = 0;
       }
     }
   }
--- byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Imaging;
 
 namespace byteArrayImageGenerator
 {
@@ -8,16 +7,20 @@ namespace byteArrayImageGenerator
   {
     static public void Main(String[] args)
     {
-      Console.WriteLine("To Test function.");
+      Console.WriteLine("For static testing function.");
 
       //Test values
       string folderPath = @"C:\Users\TKK\Documents\Onyx_projects\file.png";
-      var width = 1000;
-      var height = 700;
-      byte[] testArr = { 255, 255, 0, 255, 255, 0, 255, 255, 0, 255, 0, 255, 0, 0, 255, 255 };
+      var width = 100;
+      var height = 100;
+      // Array Output = { Yellow, Yellow, Yellow, Yellow, Blue, Blue, Blue, Blue, White, White, White, White, Red, Red, Red, Red }
+      byte[] testArr = { 255, 255, 0, 255,  255, 255, 0, 255,  255, 255, 0, 255,  255, 255, 0, 255,
+                          0, 0, 255, 255,  0, 0, 255, 255,  0, 0, 255, 255,  0, 0, 255, 255,
+                          255, 255, 255, 255,  255, 255, 255, 255,  255, 255, 255, 255,  255, 255, 255, 255,
+                          255, 0, 0, 255,  255, 0, 0, 255,  255, 0, 0, 255,  255, 0, 0, 255 };
 
       //Test #6
-      Image success = ImageGenerator.GenerateImageFromByteArr(testArr, width, height, ImageFormat.Png);
+      Image success = ImageGenerator.GenerateImageFromByteArr(testArr, width, height);
       success.Save(folderPath);
     }
   }
