877ac01f2eff4d9d1885955c0389e82378cfe947
FJvanEeden <fredz.eeden@gmail.com>
2020-05-10 17:19:16 +0200

    Fixed Array overflow bug.

--- byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
@@ -8,10 +8,10 @@ namespace byteArrayImageGenerator
 {
   class ImageGenerator
   {
-    // Made static to test
     public static Image GenerateImageFromByteArr(IEnumerable<byte> rawImageData, int width, int height)
     {
       var x = 0;
+      var y = 0;
       int i;
       byte[] pixel = new byte[4];
       byte[] newData = new byte[width * height * 4];
@@ -19,34 +19,36 @@ namespace byteArrayImageGenerator
       foreach (var data in rawImageData)
       {
         i = x % 4;
-        if (x >= 0 && i == 0)
+        if (x > 0 && i == 0)
         {
           byte r = pixel[0];
           byte g = pixel[1];
           byte b = pixel[2];
           byte a = pixel[3];
           byte[] newPixel = new byte[] { b, g, r, a };
-          Array.Copy(newPixel, 0, newData, x, 4);
+          Array.Copy(newPixel, 0, newData, y, 4);
+          y += 4;
         }
         pixel[i] = data;
         x++;
       }
 
       i = x % 4;
-      if (x >= 0 && i == 0)
+      if (x > 0 && i == 0)
       {
         byte r = pixel[0];
         byte g = pixel[1];
         byte b = pixel[2];
         byte a = pixel[3];
         byte[] newPixel = new byte[] { b, g, r, a };
-        Array.Copy(newPixel, 0, newData, x, 4);
+        Array.Copy(newPixel, 0, newData, y, 4);
       }
 
       using (Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
       {
         BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
-        Marshal.Copy(newData, 0, bmpData.Scan0, newData.Length);
+        IntPtr pNative = bmpData.Scan0;
+        Marshal.Copy(newData, 0, pNative, newData.Length -1);
 
         bmp.UnlockBits(bmpData);
         return (Image)bmp.Clone();
--- byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
@@ -1,47 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
 
 namespace byteArrayImageGenerator
 {
-    internal class Program
+  internal class Program
     {
-        static public void Main(String[] args)
-        {
-            Console.WriteLine("For static testing function.");
+    static public void Main(String[] args)
+    {
+      Console.WriteLine("For static testing function.");
 
-            //Test values
-            string folderPath = @"C:\temp\file.png";
-            var width = 100;
-            var height = 100;
-            // Array Output = { Yellow, Yellow, Yellow, Yellow, Blue, Blue, Blue, Blue, White, White, White, White, Red, Red, Red, Red }
-            byte[] testArr = { 255, 255, 0, 255,  255, 255, 0, 255,  255, 255, 0, 255,  255, 255, 0, 255,
-                          0, 0, 255, 255,  0, 0, 255, 255,  0, 0, 255, 255,  0, 0, 255, 255,
-                          255, 255, 255, 255,  255, 255, 255, 255,  255, 255, 255, 255,  255, 255, 255, 255,
-                          255, 0, 0, 255,  255, 0, 0, 255,  255, 0, 0, 255,  255, 0, 0, 255 };
+      //Test values
+      //string folderPath = @"C:\Users\TKK\Documents\Onyx_projects\file.png";
+      string folderPath = @"C:\temp\file.png";
+      var width = 1920;
+      var height = 1080;
 
-            //Test #6
-            var image = RedImage(width, height);
-            Image success = ImageGenerator.GenerateImageFromByteArr(image, width, height);
-            success.Save(folderPath);
-        }
+      //Test #12
+      var image = RedImage(width, height);
+      image.ToArray();
 
-        private static IEnumerable<byte> RedImage(int width, int height)
-        {
-            int index = 0;
-            var pixel = new byte[] { 255, 0, 0, 255 };
-            for(int x = 0; x  < width; x ++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    for (int p = 0; p < 4; p++)
-                    {
-                        yield return pixel[index % 4];
-                        index++;
-                    }
-                }
-                
-            }
-        }
+      Image success = ImageGenerator.GenerateImageFromByteArr(image, width, height);
+      success.Save(folderPath);
     }
+
+    private static IEnumerable<byte> RedImage(int width, int height)
+    {
+      int index = 0;
+      var pixel = new byte[] { 255, 0, 0, 255 };
+      for (int x = 0; x < width; x++)
+      {
+        for (int y = 0; y < height; y++)
+        {
+          for (int p = 0; p < 4; p++)
+          {
+            yield return pixel[index % 4];
+            index++;
+          }
+        }
+      }
+    }
+  }
 }
