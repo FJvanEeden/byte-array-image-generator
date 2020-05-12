7c4d0f129a538740ba59bd6fb1eeee8617a024cf
barnstokkr <reimund.rabe@gmail.com>
2020-05-12 13:02:11 +0200

    Easier array usage. Red with blue and green circle image.

--- byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/ImageGenerator.cs
@@ -2,57 +2,26 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.Linq;
 using System.Runtime.InteropServices;
 
 namespace byteArrayImageGenerator
 {
-  class ImageGenerator
-  {
-    public static Image GenerateImageFromByteArr(IEnumerable<byte> rawImageData, int width, int height)
+    internal class ImageGenerator
     {
-      var x = 0;
-      var y = 0;
-      int i;
-      byte[] pixel = new byte[4];
-      byte[] newData = new byte[width * height * 4];
-
-      foreach (var data in rawImageData)
-      {
-        i = x % 4;
-        if (x > 0 && i == 0)
+        public static Image GenerateImageFromByteArr(IEnumerable<byte> rawImageData, int width, int height)
         {
-          byte r = pixel[0];
-          byte g = pixel[1];
-          byte b = pixel[2];
-          byte a = pixel[3];
-          byte[] newPixel = new byte[] { b, g, r, a };
-          Array.Copy(newPixel, 0, newData, y, 4);
-          y += 4;
+            byte[] newData = rawImageData.ToArray();
+
+            using (Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
+            {
+                BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
+                IntPtr pNative = bmpData.Scan0;
+                Marshal.Copy(newData, 0, pNative, newData.Length - 1);
+
+                bmp.UnlockBits(bmpData);
+                return (Image)bmp.Clone();
+            }
         }
-        pixel[i] = data;
-        x++;
-      }
-
-      i = x % 4;
-      if (x > 0 && i == 0)
-      {
-        byte r = pixel[0];
-        byte g = pixel[1];
-        byte b = pixel[2];
-        byte a = pixel[3];
-        byte[] newPixel = new byte[] { b, g, r, a };
-        Array.Copy(newPixel, 0, newData, y, 4);
-      }
-
-      using (Bitmap bmp = new Bitmap(width, height, PixelFormat.Format32bppArgb))
-      {
-        BitmapData bmpData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.WriteOnly, bmp.PixelFormat);
-        IntPtr pNative = bmpData.Scan0;
-        Marshal.Copy(newData, 0, pNative, newData.Length -1);
-
-        bmp.UnlockBits(bmpData);
-        return (Image)bmp.Clone();
-      }
     }
-  }
 }
--- byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
+++ byteArrayImageGenerator/byteArrayImageGenerator/Program.cs
@@ -1,45 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 
 namespace byteArrayImageGenerator
 {
-  internal class Program
+    internal class Program
     {
-    static public void Main(String[] args)
-    {
-      Console.WriteLine("For static testing function.");
-
-      //Test values
-      //string folderPath = @"C:\Users\TKK\Documents\Onyx_projects\file.png";
-      string folderPath = @"C:\temp\file.png";
-      var width = 1920;
-      var height = 1080;
-
-      //Test #12
-      var image = RedImage(width, height);
-      image.ToArray();
-
-      Image success = ImageGenerator.GenerateImageFromByteArr(image, width, height);
-      success.Save(folderPath);
-    }
-
-    private static IEnumerable<byte> RedImage(int width, int height)
-    {
-      int index = 0;
-      var pixel = new byte[] { 255, 0, 0, 255 };
-      for (int x = 0; x < width; x++)
-      {
-        for (int y = 0; y < height; y++)
+        static public void Main(String[] args)
         {
-          for (int p = 0; p < 4; p++)
-          {
-            yield return pixel[index % 4];
-            index++;
-          }
+            Console.WriteLine("For static testing function.");
+
+            //Test values
+            //string folderPath = @"C:\Users\TKK\Documents\Onyx_projects\file.png";
+            string folderPath = @"C:\temp\file.png";
+            var width = 1920;
+            var height = 1080;
+
+            //Test #12
+            var image = RedImageWithBlueCircle(width, height);
+
+            Image success = ImageGenerator.GenerateImageFromByteArr(image, width, height);
+            success.Save(folderPath);
         }
-      }
+
+        private static IEnumerable<byte> RedImage(int width, int height)
+        {
+            int index = 0;
+            var pixel = new byte[] { 255, 0, 0, 255 };
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    for (int p = 0; p < 4; p++)
+                    {
+                        yield return pixel[index % 4];
+                        index++;
+                    }
+                }
+            }
+        }
+
+        private static IEnumerable<byte> RedImageWithBlueCircle(int width, int height)
+        {
+            int index = 0;
+            // { b, g, r, a };
+            var red = new byte[] { 0, 0, 255, 255 };
+            var blue = new byte[] { 255, 0, 0, 255 };
+            var green = new byte[] { 0, 255, 0, 255 };
+
+            int blueXOffset = 300;
+            int blueYOffset = 200;
+            int blueInnerRadius = 300;
+            int blueOuterRadius = 500;
+
+            int greenXOffset = 800;
+            int greenYOffset = 700;
+            int greenInnerRadius = 400;
+            int greenOuterRadius = 500;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (((x - blueXOffset) * (x - blueXOffset) + (y - blueYOffset) * (y - blueYOffset) > blueInnerRadius * blueInnerRadius) && ((x - blueXOffset) * (x - blueXOffset) + (y - blueYOffset) * (y - blueYOffset) < blueOuterRadius * blueOuterRadius))
+                    {
+                        for (int p = 0; p < 4; p++)
+                        {
+                            yield return blue[index % 4];
+                            index++;
+                        }
+                    }
+                    else if (((x - greenXOffset) * (x - greenXOffset) + (y - greenYOffset) * (y - greenYOffset) > greenInnerRadius * greenInnerRadius) && ((x - greenXOffset) * (x - greenXOffset) + (y - greenYOffset) * (y - greenYOffset) < greenOuterRadius * greenOuterRadius))
+                    {
+                        for (int p = 0; p < 4; p++)
+                        {
+                            yield return green[index % 4];
+                            index++;
+                        }
+                    }
+                    else
+                    {
+                        for (int p = 0; p < 4; p++)
+                        {
+                            yield return red[index % 4];
+                            index++;
+                        }
+                    }
+                }
+            }
+        }
     }
-  }
 }
